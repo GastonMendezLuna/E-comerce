a9da2494005c20d2380ef65739f5849b04ebb849
gaston mendez luna <mendezlunagaston@gmail.com>
2022-02-03 21:15:51 -0300

    manue navegacion ok

---
+++ javascript/ecomerce.js
@@ -0,0 +1,16 @@
+let nombreProductoA = 'mesa';
+let stockProductoA = 10;
+let precioProductoA = 10.000;
+
+let nombreProductoB = 'silla';
+let stockProductoA = 20;
+let precioProductoA = 2.000;
+
+let nombreCompra = prompt("ingrese el nombre del producto que quiere comprar:");
+let cantidadComprada = parseInt(prompt("ingrese la cantidad que quiere comprar"));
+
+if (nombreCompra == nombreProductoA)
+
+
+
+
