5865ab2fc6e1a6c8bf53de56a4fd44f1470c36f2
gaston mendez luna <mendezlunagaston@gmail.com>
2022-02-15 23:04:29 -0300

    cargando ciclo javascript trabajo practico 1

--- javascript/ciclo.js
+++ javascript/ciclo.js
@@ -1,73 +1,36 @@
 
 
-let nombre = prompt("¿Cómo es tu nombre?");
-let apellido = prompt("¿Cómo es tu apellido?");
-alert("¡¡Hola" + " " + nombre + " " + apellido + " " + "!!");
-let edad = parseInt(prompt("¿Qué edad tienes?"));
-let fecha = 2021;
-let anioNacimiento = fecha - edad;
-alert("Tu fecha de nacimiento es mas o menos en el año " + anioNacimiento);
-
-
-let productoA = "cesto mimbre";
-let stockA = 10;
-let precioA = 3000;
-let descuentoProductoA = precioA * 0.10;
-
-let productoB = "bolso rustico";
-let stockB = 10;
-let precioB = 2500;
-let descuentoProductoB = precioB *0.10;
-
-let productoC = "atrapa sueños";
-let stockC = 10;
-let precioC = 2000;
-let descuentoProductoC = precioC * 0.10;
-
-
-let productoD = "cuadro tres partes";
-let stockD = 4;
-let precioD = 3000;
-let descuentoProductoD = precioD * 0.10;
-
-let compra = prompt("¿Qué producto deseas comprar?");
-
-if (compra == productoA){
-  let cantidadComprada = parseInt(prompt("¿cuantas unidades?"));
-  let montoApagar = cantidadComprada * precioA - descuentoProductoA * cantidadComprada;
-  stock = stockA - cantidadComprada;
-
-alert("Pago en efectivo tiene 10% de descuento. Por lo que pagaras " + montoApagar + " pesos");
-  console.log("te quedan en stock " + stock);
+let elegido = "tijera";
+quiereJugar = prompt("Quiere jugar? si/no");
+while (quiereJugar == "si")  {
+    ingresoUsuario = prompt("piedra, papel o tijera?");
+    switch (ingresoUsuario) {
+    case "tijera":
+    alert("Ganaste!");
+    break;
+    case "piedra":
+    alert("Perdiste");
+    break;
+    case "papel":
+    alert("Perdiste");
+    break;
+    
 }
+quiereJugar = prompt("Quiere jugar? si/no. Escriba no en caso de que se haya aburrido");
 
-else if (compra == productoB){
-  let cantidadComprada = parseInt(prompt("¿cuantas unidades?"));
-  let montoApagar = cantidadComprada * precioB - descuentoProductoB * cantidadComprada;
-  stock = stockB - cantidadComprada;
-
-alert("Pago en efectivo tiene 10% de descuento. Por lo que pagaras " + montoApagar + " pesos");
-  console.log("te quedan en stock " + stock);
 }
-
-else if (compra == productoC){
-  let cantidadComprada = parseInt(prompt("¿cuantas unidades?"));
-  let montoApagar = cantidadComprada * precioC - descuentoProductoC * cantidadComprada;
-  stock = stockC - cantidadComprada;
-
-alert("Pago en efectivo tiene 10% de descuento. Por lo que pagaras " + montoApagar + " pesos");
-  console.log("te quedan en stock " + stock);
-}
-
-else if (compra == productoD){
-  let cantidadComprada = parseInt(prompt("¿cuantas unidades?"));
-  let montoApagar = cantidadComprada * precioD - descuentoProductoD * cantidadComprada;
-  stock = stockD - cantidadComprada;
-
-alert("Pago en efectivo tiene 10% de descuento. Por lo que pagaras " + montoApagar + " pesos");
-  console.log("te quedan en stock " + stock);
-}
-
-
-
  
+let Jugando = prompt("Queres seguir jugando? Bueno/no, sos pesado")
+for (let i = 2; i <= 1000; i++) {
+alert(i + " elefantes se columpiaban sobre la tela de una araña");
+}
+
+
+
+
+
+
+
+
+
+
--- javascript/ecomerce.js
+++ javascript/ecomerce.js
@@ -11,3 +11,88 @@ $(".menuResponsive").toggle(300).addclass('.mostrar');
 
 
 });
+
+/*let nombre = prompt("¿Cómo es tu nombre?");
+let apellido = prompt("¿Cómo es tu apellido?");
+alert("¡¡Hola" + " " + nombre + " " + apellido + " " + "!!");
+alert("Los productos que tenemos en venta son: cesto mimbre, bolso rustico, atrapa sueños y cuadro tres partes ")
+
+
+let productoA = "cesto mimbre";
+let stockA = 10;
+let precioA = 3000;
+let descuentoProductoA = precioA * 0.10;
+
+let productoB = "bolso rustico";
+let stockB = 10;
+let precioB = 2500;
+let descuentoProductoB = precioB *0.10;
+
+let productoC = "atrapa sueños";
+let stockC = 10;
+let precioC = 2000;
+let descuentoProductoC = precioC * 0.10;
+
+
+let productoD = "cuadro tres partes";
+let stockD = 10;
+let precioD = 3000;
+let descuentoProductoD = precioD * 0.10;
+
+let compra = prompt("¿Qué producto deseas comprar?");
+
+if (compra == productoA){
+  let cantidadComprada = parseInt(prompt("¿cuantas unidades?"));
+  if(cantidadComprada <= stockA){
+  stock = stockA - cantidadComprada;
+  let montoApagar = cantidadComprada * precioA - descuentoProductoA * cantidadComprada;
+  
+alert("Pago en efectivo tiene 10% de descuento. Por lo que pagaras " + montoApagar + " pesos");
+  console.log("te quedan en stock " + stock);
+    }
+else{
+  alert("Solo puedes comprar " + stockA + " unidades de este producto. Muchas gracias")
+} 
+}
+
+else if (compra == productoB){
+  let cantidadComprada = parseInt(prompt("¿cuantas unidades?"));
+  if(cantidadComprada <= stockB){
+  stock = stockB - cantidadComprada;
+  let montoApagar = cantidadComprada * precioB - descuentoProductoB * cantidadComprada;
+  
+alert("Pago en efectivo tiene 10% de descuento. Por lo que pagaras " + montoApagar + " pesos");
+  console.log("te quedan en stock " + stock);
+    }
+else{
+  alert("Solo puedes comprar " + stockB + " unidades de este producto. Muchas gracias")
+} 
+}
+
+else if (compra == productoC){
+  let cantidadComprada = parseInt(prompt("¿cuantas unidades?"));
+  if(cantidadComprada <= stockC){
+  stock = stockC - cantidadComprada;
+  let montoApagar = cantidadComprada * precioC - descuentoProductoC * cantidadComprada;
+  
+alert("Pago en efectivo tiene 10% de descuento. Por lo que pagaras " + montoApagar + " pesos");
+  console.log("te quedan en stock " + stock);
+    }
+else{
+  alert("Solo puedes comprar " + stockC + " unidades de este producto. Muchas gracias")
+} 
+}
+
+else if (compra == productoD){
+  let cantidadComprada = parseInt(prompt("¿cuantas unidades?"));
+  if(cantidadComprada <= stockD){
+  stock = stockD - cantidadComprada;
+  let montoApagar = cantidadComprada * precioD - descuentoProductoD * cantidadComprada;
+  
+alert("Pago en efectivo tiene 10% de descuento. Por lo que pagaras " + montoApagar + " pesos");
+  console.log("te quedan en stock " + stock);
+    }
+else{
+  alert("Solo puedes comprar " + stockD + " unidades de este producto. Muchas gracias")
+} 
+}*/
